2d0d97cfd9a1e0f94546668bd3ff67fd41f1e1e8
Maksim Volkov <maksan.russ@mail.ru>
2025-06-29 12:48:46 +0300

    Create registration and screen of auth

--- ShopClient/ShopClient/Controller/WelcomeViewController.swift
+++ ShopClient/ShopClient/Controller/WelcomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  WelcomeViewController.swift
 //  ShopClient
 //
 //  Created by Maksim Volkov on 09.05.2025.
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WelcomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
 
 }
 
---
+++ ShopClient/ShopClient/Network/RegistrationService.swift
@@ -0,0 +1,83 @@
+//
+//  RegistrationService.swift
+//  ShopClient
+//
+//  Created by Maksim Volkov on 28.06.2025.
+//
+
+import Foundation
+
+final class RegistrationService {
+    
+    private let scheme = "https"
+    private let host = "shopswift.volkovmd.ru"
+    
+    private let session: URLSession = {
+        let config = URLSessionConfiguration.default
+        let session = URLSession(configuration: config)
+        return session
+    }()
+    
+    func request(username: String, email: String, password: String) -> String {
+        
+        var resultMessage = ""
+        // создаем словарь для отправки данных пользователя на сервер
+        let param: [String: Any] = ["username": username,
+                                    "email": email,
+                                    "password": password
+        ]
+        
+        // добавляем метод для регистрации
+        let url = configureUrl(method: "/registration")
+        print(url)
+        
+        // создаем data с данными пользователя
+        let jsonData = try? JSONSerialization.data(withJSONObject: param)
+        
+        // create post request
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        
+        // нужный способ передачи данных на Vapor
+        request.httpBody = jsonData
+        // без этого указания Vapor будет писать "Bad request"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let task = session.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                print(error.localizedDescription)
+            }
+            guard let data = data else { return }
+            let decoder = JSONDecoder()
+            
+            do {
+                let result = try decoder.decode(ServerResponseReg.self, from: data)
+                resultMessage = result.user_message
+                print(resultMessage)
+            } catch {
+                print(error)
+            }
+        }
+        task.resume()
+        sleep(1)
+        return resultMessage
+    }
+}
+
+
+private extension RegistrationService {
+    
+    func configureUrl(method: String) -> URL {
+        
+        var urlComponents = URLComponents()
+        urlComponents.scheme = scheme
+        urlComponents.host = host
+//        urlComponents.port = port
+        urlComponents.path = method
+        
+        guard let url = urlComponents.url else {
+            fatalError("URL is invalid")
+        }
+        return url
+    }
+}
